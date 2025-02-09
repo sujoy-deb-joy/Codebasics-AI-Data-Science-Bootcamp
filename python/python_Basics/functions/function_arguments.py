0157e71e64e031a83d09bcd8c07403b5b847f28a
Sujoy deb <79310660+sujoy-deb-joy@users.noreply.github.com>
2025-02-09 00:11:22 +0000

    a gentle introduction to function

---
+++ python/python_Basics/functions/function_arguments.py
@@ -0,0 +1,19 @@
+def sum_all(*args):
+    total = 0
+    for item in args:
+        total += item
+    return total
+
+total = sum_all(1,2,3,4,5)
+
+print(f"Sum of all numbers: {total}")    
+
+def company_info(**kwargs):
+    if "ticker" in kwargs:
+        print("Ticker: ", kwargs['ticker'])
+    if "ceo" in kwargs:
+        print("CEO: ", kwargs['ceo'])
+    if "revenue" in kwargs:
+        print("Revenue: ", kwargs['revenue'])        
+
+company_info(ticker='AAPL', ceo="Tim Cook", revenue="200 billion")        
